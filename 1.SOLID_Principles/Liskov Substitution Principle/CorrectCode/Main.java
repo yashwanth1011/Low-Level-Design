b0aa5e33bdb5723022f6849a4e442c6114c729d8
yashwanth1011 <yashwanthchennu1999@gmail.com>
2024-10-19 21:47:54 -0400

    Adding LSP final Code

--- 1.SOLID_Principles/Liskov Substitution Principle/CorrectCode/Main.java
+++ 1.SOLID_Principles/Liskov Substitution Principle/CorrectCode/Main.java
@@ -1,9 +1,13 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        SmartPhone iphone = new Iphone();
-        SmartPhone samsung = new Samsung();
-        
-        iphone.touchFeatures();
-        samsung.touchFeatures();
+        List<PhoneLSP> phones = new ArrayList<>();
+        phones.add(new DialPhone());
+        phones.add(new SmartPhone());
+        phones.add(new Iphone());
+        phones.add(new Samsung());
+        for(PhoneLSP phone: phones){
+            phone.playGames();
+        }
     }
 }
